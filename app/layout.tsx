6ff5c71fb5cb5e833410e2ce6490a783dbc563ab
pingfury <pingfury@outlook.com>
2024-06-07 17:04:47 +0800

    fix: ui
    - add nextui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-    title: "钱条",
-    description: "挣钱的进度条，得是老板给我的欠条。",
+  title: "钱条",
+  description: "挣钱的进度条，得是老板给我的欠条。",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className} >{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" className='dark'>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { Time } from "@internationalized/date";
+import { Checkbox } from "@nextui-org/checkbox";
+import { Input } from "@nextui-org/input";
 import { Progress } from "@nextui-org/progress";
 
 export default function Home() {
@@ -10,10 +13,10 @@ export default function Home() {
         挣钱的进度条，得是老板给我的欠条。
       </div>
       <div className="container">
-        <div className="grid grid-cols-4 gap-2 py-1">
-          <Progress color="primary" aria-label="Loading..." value={99}
-            size="sm"
-            radius="sm"
+        <div className="container  grid grid-cols-4 gap-2 py-1">
+          <Progress color="primary" aria-label="Loading..." value={55}
+            size="lg"
+            radius="lg"
             className="max-w-md col-start-2 col-span-2"
             showValueLabel={true}
             disableAnimation={true}
@@ -21,57 +24,34 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-4 gap-2 py-1">
           <div className="container col-end-3">
-            <div className="grid grid-cols-2">
-              <label >上班于</label>
-              <input type="time" defaultValue={"08:00"}></input>
-            </div>
+            <Input label="上班于" labelPlacement="outside-left" type="time" defaultValue={"08:00"} />
           </div>
           <div className="container col-start-3">
-            <div className="grid grid-cols-2">
-              <label>下班于</label>
-              <input type="time" defaultValue={"19:00"}></input>
-            </div>
+            <Input label="下班于" labelPlacement="outside-left" type="time" defaultValue={"19:00"} />
           </div>
         </div>
         <div className="grid grid-cols-4 gap-2 py-1">
-          <div className="col-end-3">
-            <input type="checkbox"></input>
-            <span>是否有午休</span>
+          <div className="col-end-3 flex items-center">
+            <Checkbox defaultSelected radius="sm" size="sm" className="">是否有午休</Checkbox>
           </div>
         </div>
         <div className="grid grid-cols-4 gap-2 py-1">
           <div className="container col-end-3">
-            <div className="grid grid-cols-2">
-              <label>午休开始于</label>
-              <input type="time" defaultValue={"12:00"}></input>
-            </div>
+            <Input label="午休开始于" labelPlacement="outside-left" type="time" defaultValue={"12:00"} />
           </div>
           <div className="container col-start-3">
-            <div className="grid grid-cols-2">
-              <label>午休结束于</label>
-              <input type="time" defaultValue={"14:00"}></input>
-            </div>
+            <Input label="午休结束于" labelPlacement="outside-left" type="time" defaultValue={"14:00"} />
           </div>
         </div>
 
         <div className="grid grid-cols-4 gap-2 py-1">
           <div className="container col-end-3">
-            <div className="grid grid-cols-2">
-              <span>月薪</span>
-              <div className="grid grid-cols-5 border rounded border-dashed">
-                <input type="number" defaultValue={3000} className="col-span-3 border-none"></input>
-                <span>CNY</span>
-              </div>
+            <div className="">
+              <Input label="月薪" labelPlacement="outside-left" type="number" placeholder="3000" />
             </div>
           </div>
           <div className="container col-start-3">
-            <div className="grid grid-cols-2">
-              <span>一个月工作</span>
-              <div className="grid grid-cols-5 border rounded border-dashed">
-                <input type="number" defaultValue={20} className="col-span-4 border-none"></input>
-                <span>天</span>
-              </div>
-            </div>
+            <Input label="工作天数" type="number" labelPlacement="outside-left" placeholder="20" />
           </div>
         </div>
       </div >
