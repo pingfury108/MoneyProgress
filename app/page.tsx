dd0643f15dea236a4d043de0b60b0a1854dc9a99
pingfury <pingfury@outlook.com>
2024-06-18 16:58:09 +0800

    fix app ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,57 @@
-import { Time } from "@internationalized/date";
+'use client'
+
 import { Checkbox } from "@nextui-org/checkbox";
 import { Input } from "@nextui-org/input";
 import { Progress } from "@nextui-org/progress";
+import React, { useState } from "react";
+
+interface Cfg {
+  start_work_time: string
+  end_work_time: string
+  lunch: boolean
+  start_lunch_time: string
+  end_lunch_time: string
+  monthly_salary: number
+  work_day: number
+}
 
 export default function Home() {
+  const cfg: Cfg = {
+    start_work_time: "08:00",
+    end_work_time: "19:00",
+    lunch: false,
+    start_lunch_time: "12:00",
+    end_lunch_time: "14:00",
+    monthly_salary: 3000,
+    work_day: 20,
+  }
+
+  const [start_work_time, setStartWorkTime] = useState(cfg.start_work_time)
+  const [end_work_time, setEndWorkTime] = useState(cfg.end_work_time)
+  const [lunch, setLunch] = useState(cfg.lunch)
+  const [start_lunch_time, setStartLunchTime] = useState(cfg.start_lunch_time)
+  const [end_lunch_time, setEndLunchTime] = useState(cfg.end_lunch_time)
+  const [monthly_salary, setMonthlySalary] = useState(cfg.monthly_salary)
+  const [work_day, setWorkDay] = useState(cfg.work_day)
+
+
+  const LunchHtml = () => {
+    if (lunch) {
+      return (
+        <div className="grid grid-cols-4 gap-2 py-1">
+          <div className="container col-end-3">
+            <Input label="午休开始于" labelPlacement="outside-left" type="time" defaultValue={"12:00"} value={start_lunch_time} onChange={(event) => { setStartLunchTime(event?.target.value) }} />
+          </div>
+          <div className="container col-start-3">
+            <Input label="午休结束于" labelPlacement="outside-left" type="time" defaultValue={"14:00"} value={end_lunch_time} onChange={(event) => { setEndLunchTime(event?.target.value) }} />
+          </div>
+        </div>
+      )
+    }
+    return null
+  }
+
+
   return (
     <main className="container">
       <div className="container text-center pt-10">
@@ -24,34 +72,26 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-4 gap-2 py-1">
           <div className="container col-end-3">
-            <Input label="上班于" labelPlacement="outside-left" type="time" defaultValue={"08:00"} />
+            <Input label="上班于" labelPlacement="outside-left" type="time" defaultValue={"08:00"} value={start_work_time} onChange={(event) => { setStartWorkTime(event?.target.value) }} />
           </div>
           <div className="container col-start-3">
-            <Input label="下班于" labelPlacement="outside-left" type="time" defaultValue={"19:00"} />
+            <Input label="下班于" labelPlacement="outside-left" type="time" defaultValue={"19:00"} value={end_work_time} onChange={(event) => { setEndWorkTime(event?.target.value) }} />
           </div>
         </div>
         <div className="grid grid-cols-4 gap-2 py-1">
           <div className="col-end-3 flex items-center">
-            <Checkbox defaultSelected radius="sm" size="sm" className="">是否有午休</Checkbox>
+            <Checkbox radius="sm" size="sm" value={String(lunch)} onChange={(event) => { setLunch(event.target.checked) }}>是否有午休</Checkbox>
           </div>
         </div>
-        <div className="grid grid-cols-4 gap-2 py-1">
-          <div className="container col-end-3">
-            <Input label="午休开始于" labelPlacement="outside-left" type="time" defaultValue={"12:00"} />
-          </div>
-          <div className="container col-start-3">
-            <Input label="午休结束于" labelPlacement="outside-left" type="time" defaultValue={"14:00"} />
-          </div>
-        </div>
-
+        <LunchHtml />
         <div className="grid grid-cols-4 gap-2 py-1">
           <div className="container col-end-3">
             <div className="">
-              <Input label="月薪" labelPlacement="outside-left" type="number" placeholder="3000" />
+              <Input label="月薪" labelPlacement="outside-left" type="number" placeholder="3000" value={monthly_salary.toString()} onChange={(event) => { setMonthlySalary(Number(event?.target.value)) }} />
             </div>
           </div>
           <div className="container col-start-3">
-            <Input label="工作天数" type="number" labelPlacement="outside-left" placeholder="20" />
+            <Input label="工作天数" type="number" labelPlacement="outside-left" placeholder="20" value={work_day.toString()} onChange={(event) => { setWorkDay(Number(event?.target.value)) }} />
           </div>
         </div>
       </div >
